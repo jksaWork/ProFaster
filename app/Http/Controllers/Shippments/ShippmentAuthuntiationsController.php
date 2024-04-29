18d50b3550f4348743bc8397b1a77bd3bc96c8f9
jksa.work.1@gmail.com <jksa.work.1@gmail.com>
2024-04-29 13:55:25 -0400

    Provider Data

--- app/Http/Controllers/Shippments/ShippmentAuthuntiationsController.php
+++ app/Http/Controllers/Shippments/ShippmentAuthuntiationsController.php
@@ -19,7 +19,7 @@ class ShippmentAuthuntiationsController extends Controller
             'redirectUri'  => 'https://salla.proofast.com/webhock' , // the url for current page in your service
         ]);
      
-        dd("Hi There");
+        dd($provider);
         if (!isset($_GET['code']) || empty($_GET['code'])) {
             // If we don't have an authorization code then get one
             $authUrl = $provider->getAuthorizationUrl([
