6b391b3895eb332c51a0927e4bace2a09acdb0df
jksa.work.1@gmail.com <jksa.work.1@gmail.com>
2024-04-30 03:35:32 -0400

    Suppose To Send Request

--- app/Listeners/UpdateShippemntsEventListner.php
+++ app/Listeners/UpdateShippemntsEventListner.php
@@ -28,7 +28,6 @@ class UpdateShippemntsEventListner
     public function handle($event)
     {
 
-        dd($event->order , $event->sallaOrder);
         $headers = [
             'Content-Type' => 'application/json'
          ];
