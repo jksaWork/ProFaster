e5e6eece82030c6a4d2ca087b7a867a6201b67fc
jksa.work.1@gmail.com <jksa.work.1@gmail.com>
2024-05-01 06:23:01 -0400

    Use Pdf

--- app/Listeners/UpdateShippemntsEventListner.php
+++ app/Listeners/UpdateShippemntsEventListner.php
@@ -3,7 +3,7 @@
 namespace App\Listeners;
 
 use App\Models\Order;
-use Barryvdh\DomPDF\PDF;
+use PDF;
 use HttpClient;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
