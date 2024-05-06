2475d22b0e2e71eb28adf9f51588dc3de63ff4b1
jksa.work.1@gmail.com <jksa.work.1@gmail.com>
2024-05-06 04:52:08 -0400

    Add Dev Credtainals

--- app/Http/Controllers/Shippments/ShippmentAuthuntiationsController.php
+++ app/Http/Controllers/Shippments/ShippmentAuthuntiationsController.php
@@ -16,15 +16,13 @@ class ShippmentAuthuntiationsController extends Controller
         info("Inter The Web Hock Code");
         // for dev -----------------------------
         $provider = new Salla([
-            'clientId'     =>   'cbc3a856-8fd1-4452-9ccc-b7bd41081969' , #'c5e26ae228c097732386852c0194ade7', // The client ID assigned to you by Salla
-            'clientSecret' =>  '7e4071235c11bd8bf81ad3d8914e8bf2' ,  #'470e3ce6a091ce4a43fe30be1792313c', // The client password assigned to you by Salla
-            'redirectUri'  => 'https://salla.proofast.com/webhook' , // the url for current page in your service
+            'clientId'     => '7fb2038a-e26e-4244-8d5a-343354f21b8d' , ## cbc3a856-8fd1-4452-9ccc-b7bd41081969 // Productio Credtaional
+            'clientSecret' => '5e0396f5884611a73db7cc71732d2f11' , ## '7e4071235c11bd8bf81ad3d8914e8bf2' ,  
+            'redirectUri'  => 'https://salla.proofast.com/webhook' ,
         ]);
      
         info("Inter Affter Get Profile Id");
 
-
-        // dd($provider);
 
         if (!isset($_GET['code']) || empty($_GET['code'])) {
             // If we don't have an authorization code then get one
@@ -55,10 +53,12 @@ class ShippmentAuthuntiationsController extends Controller
                 $Client = Client::create([
                     'fullname' => $user->getName() ,
                     'phone' => $user->getMobile(),
+                    'email' => $user->getEmail(),
                     'area_id' => 1, 
                     'sub_area_id' => 1, 
                     'enable_salla_shippments' => true, 
-                    'client_type' => 'salla_acount', 
+                    'client_type' => 'salla_acount',
+                    'password' => bcrypt($user->getEmail()) 
                 ]);
             }
                 
